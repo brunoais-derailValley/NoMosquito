8a3f47b122920c4eb1a8d2ad7b9ecf35bbaa9d04
brunoais <brunoaiss@gmail.com>
2023-08-29 18:23:29 +0100

    First working version

--- NoMosquito/Main.cs
+++ NoMosquito/Main.cs
@@ -1,16 +1,25 @@
 ﻿using UnityEngine;
-using UnityModManagerNet;
 using System.Reflection;
+using System;
+using UnityModManagerNet;
 using HarmonyLib;
+using System.Text.RegularExpressions;
 
 namespace NoMosquito
 {
     public class Main
     {
         public static bool enabled;
+        public static int disableCount = 0;
         public static UnityModManager.ModEntry mod;
 
-        static bool Load(UnityModManager.ModEntry modEntry)
+        public static Regex beeMatch;
+
+        static Main(){
+            beeMatch = new Regex(@"Env-Insect-(?:Bee|Mosquito)[0-9]{1,2}", RegexOptions.Compiled);
+        }
+
+        public static bool Load(UnityModManager.ModEntry modEntry)
         {
             var harmony = new Harmony(modEntry.Info.Id);
             harmony.PatchAll(Assembly.GetExecutingAssembly());
@@ -30,14 +39,26 @@ namespace NoMosquito
         }
     }
 
-    [HarmonyPatch(typeof(UnityEngine.AudioSource))]
-    [HarmonyPatch(nameof(UnityEngine.AudioSource.PlayOneShot))]
+    [HarmonyPatch(typeof(UnityEngine.AudioSource), nameof(UnityEngine.AudioSource.PlayOneShot), new[]{typeof(UnityEngine.AudioClip), typeof(System.Single)})]
     static class NoMosquitoSoundPatch
     {
-        static bool Prefix(ref UnityEngine.AudioClip clip, ref System.Single volume)
+        static bool Prefix(ref UnityEngine.AudioClip clip, ref System.Single volumeScale)
         {
-            Debug.Log($"[NoMosquito]: Audio name: {clip.name} at volume {volume}");
-            return true;
+            if(!Main.enabled){
+                if(Main.disableCount == 0){
+                    Debug.Log("[NoMosquito] disabled");
+                }
+                Main.disableCount = Main.disableCount + 1 % 100;
+                return true;
+            }
+
+            var averted = "";
+            if(Main.beeMatch.IsMatch(clip.name)){
+                averted = " averted";
+                Debug.Log($"[NoMosquito]{averted}: Audio name: {clip.name} at volume {volumeScale}");
+            }
+
+            return averted == "";
         }
     }
 }
