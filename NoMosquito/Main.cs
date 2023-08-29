dc8e34040060f1942fa5110e44a72f4ae37c37fb
brunoais <brunoaiss@gmail.com>
2023-08-29 12:41:23 +0100

    First compiled version

---
+++ NoMosquito/Main.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityModManagerNet;
+using System.Reflection;
+using HarmonyLib;
+
+namespace NoMosquito
+{
+    public class Main
+    {
+        public static bool enabled;
+        public static UnityModManager.ModEntry mod;
+
+        static bool Load(UnityModManager.ModEntry modEntry)
+        {
+            var harmony = new Harmony(modEntry.Info.Id);
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+
+            mod = modEntry;
+            modEntry.OnToggle = OnToggle;
+
+            return true;
+        }
+
+        static bool OnToggle(UnityModManager.ModEntry modEntry, bool value)
+        {
+            enabled = value;
+            modEntry.Logger.Log(Application.loadedLevelName);
+
+            return true;
+        }
+    }
+
+    [HarmonyPatch(typeof(UnityEngine.AudioSource))]
+    [HarmonyPatch(nameof(UnityEngine.AudioSource.PlayOneShot))]
+    static class NoMosquitoSoundPatch
+    {
+        static bool Prefix(ref UnityEngine.AudioClip clip, ref System.Single volume)
+        {
+            Debug.Log($"[NoMosquito]: Audio name: {clip.name} at volume {volume}");
+            return true;
+        }
+    }
+}
